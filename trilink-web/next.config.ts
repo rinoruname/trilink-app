b6aabba8a56bfb026e0547ceb3dbea3f65a98c4a
Rinor Krasniqi <rinor.kraasniqi@gmail.com>
2025-02-21 13:56:47 +0100

    new push

--- trilink-web/next.config.ts
+++ trilink-web/next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  /*assetPrefix: process.env.ENV === 'local' ? '' : "https://assets.trilink.dev"*/
-  assetPrefix: process.env.ENV === 'local' ? '' : "http://localhost"
+  //assetPrefix: process.env.ENV === 'local' ? '' : "https://assets.trilink.dev"
+     assetPrefix: process.env.ENV === 'local' ? '' : "http://localhost"
 };
 
 export default nextConfig;
